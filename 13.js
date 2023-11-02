22c201909c5a6b559181acaf31f133a02b26c9c7
Itzel Perez <a355228@uach.mx>
2023-11-01 21:38:13 -0600

    Ejercicio 13

---
+++ 13.js
@@ -0,0 +1,14 @@
+contadorP=0;
+contadorIP=0;
+    for(i=0;i<=101;i++){
+        if(i%3==0){
+            console.log("Múltiplo de 2: " + i);
+            contadorP+=1;
+        }else{
+            console.log("Múltiplo de 3: " + i);
+            contadorIP+=1;
+        }
+    
+    }
+    console.log("Cantidad de números múltiplos de 3--> " + contadorP);
+    console.log("Cantidad de números múltiplos de 2--> " + contadorIP);
